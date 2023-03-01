a0af1f9fec58c23c57e82ebcf5d2392a8f1b1917
Gabriel <gabrielnaldi.castro@gmail.com>
2023-03-01 20:02:25 -0300

    update notification list route and added create route

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
 
 @Controller('notifications')
 export class AppController {
     constructor(private readonly prisma: PrismaService) {}
 
     @Get()
-    getHello() {
-        return this.prisma.notification.findMany();
+    async list() {
+        const notificationList = await this.prisma.notification.findMany();
+
+        return notificationList;
+    }
+
+    @Post()
+    async create() {
+        await this.prisma.notification.create({
+            data: {
+                id: randomUUID(),
+                category: 'Financeiro',
+                content: 'Você recebeu uma nova notificação',
+                recipientId: randomUUID(),
+            },
+        });
     }
 }
