5554da601e759626c62a82f8f86c929c9ab97c57
Gabriel N <gabrielnaldi.castro@gmail.com>
2023-07-17 21:19:09 -0300

    feat: create notification entity

---
+++ src/application/entities/notification.ts
@@ -0,0 +1,61 @@
+import { Replace } from 'src/helpers/Replace';
+import { NotificationContent } from './notification-content';
+
+interface NotificationProps {
+  recipientId: string;
+  category: string;
+  content: NotificationContent;
+  createdAt: Date;
+  readAt?: Date | null;
+}
+
+export class Notification {
+  private _props: NotificationProps;
+
+  constructor(props: Replace<NotificationProps, { createdAt?: Date }>) {
+    this._props = {
+      ...props,
+      createdAt: props.createdAt ?? new Date(),
+    };
+  }
+
+  public get recipientId() {
+    return this._props.recipientId;
+  }
+
+  public set recipientId(recipientId: string) {
+    this._props.recipientId = recipientId;
+  }
+
+  public get category() {
+    return this._props.category;
+  }
+
+  public set category(category: string) {
+    this._props.category = category;
+  }
+
+  public get content() {
+    return this._props.content;
+  }
+
+  public set content(content: NotificationContent) {
+    this._props.content = content;
+  }
+
+  public get createdAt() {
+    return this._props.createdAt;
+  }
+
+  public set createdAt(createdAt: Date) {
+    this._props.createdAt = createdAt;
+  }
+
+  public get readAt() {
+    return this._props.readAt;
+  }
+
+  public set readAt(readAt: Date) {
+    this._props.readAt = readAt;
+  }
+}
