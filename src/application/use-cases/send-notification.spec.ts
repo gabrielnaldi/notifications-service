8d47de0eed4fb9498b033eabce043144e2686a45
Gabriel N <gabrielnaldi.castro@gmail.com>
2023-07-19 20:22:54 -0300

    test: update send notification tests

--- src/application/use-cases/send-notification.spec.ts
+++ src/application/use-cases/send-notification.spec.ts
@@ -1,15 +1,25 @@
+import { Notification } from '../entities/notification';
+import { NotificationRepository } from '../repositories/notification-repository';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationRepository: NotificationRepository = {
+  async create(notification) {
+    notifications.push(notification);
+  },
+};
 
 describe('SendNotification', () => {
   it('should be able to send notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationRepository);
 
-    const notification = await sendNotification.execute({
+    await sendNotification.execute({
       recipientId: 'recipient-id-test',
       content: 'Send notification content',
       category: 'social',
     });
 
-    expect(notification).toBeTruthy();
+    expect(notifications).toHaveLength(1);
   });
 });
