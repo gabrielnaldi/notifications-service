00950325b459d87dfc78e05cd75e7b00e1fe35ca
Gabriel N <gabrielnaldi.castro@gmail.com>
2023-07-24 20:22:55 -0300

    feat: add count by recipient id method on notifications repository

--- src/application/repositories/notification-repository.ts
+++ src/application/repositories/notification-repository.ts
@@ -4,4 +4,5 @@ export abstract class NotificationRepository {
   abstract create(notification: Notification): Promise<void>;
   abstract save(notification: Notification): Promise<void>;
   abstract findById(notificationId: string): Promise<Notification | null>;
+  abstract countManyByRecipientId(recipientId: string): Promise<number>;
 }
