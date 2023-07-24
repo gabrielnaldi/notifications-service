fd87beb905284c8ee6289471677fc24d1f16397b
Gabriel N <gabrielnaldi.castro@gmail.com>
2023-07-24 19:20:35 -0300

    refactor: add cancel notification response type

--- src/application/use-cases/cancel-notification.ts
+++ src/application/use-cases/cancel-notification.ts
@@ -4,12 +4,16 @@ import { NotificationNotFound } from './errors/notification-not-found';
 interface CancelNotificationRequest {
   notificationId: string;
 }
+
+type CancelNotificationResponse = Promise<void>;
 export class CancelNotification {
   constructor(
     private readonly notificationRepository: NotificationRepository,
   ) {}
 
-  public async execute(request: CancelNotificationRequest) {
+  public async execute(
+    request: CancelNotificationRequest,
+  ): CancelNotificationResponse {
     const { notificationId } = request;
 
     const notification = await this.notificationRepository.findById(
