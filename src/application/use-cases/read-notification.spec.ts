e66ad6ae3a66ba9b9ada66df7330b3d86046abb7
Gabriel N <gabrielnaldi.castro@gmail.com>
2023-07-25 10:13:18 -0300

    test: ensure it can read a notification

---
+++ src/application/use-cases/read-notification.spec.ts
@@ -0,0 +1,20 @@
+import { InMemoryNotificationRepository } from '@test/repositories/in-memory-notification-repository';
+import { ReadNotification } from './read-notification';
+import { makeNotification } from '@test/factories/notification-factory';
+
+describe('Read notification', () => {
+  it('should be able to read notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const readNotification = new ReadNotification(notificationRepository);
+
+    const notification = makeNotification();
+
+    await notificationRepository.create(notification);
+
+    await readNotification.execute({ notificationId: notification.id });
+
+    expect(notificationRepository.notifications[0].readAt).toEqual(
+      expect.any(Date),
+    );
+  });
+});
