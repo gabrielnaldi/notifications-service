2e0033cddbb37923c30744ca7453940ea7082790
Gabriel N <gabrielnaldi.castro@gmail.com>
2023-07-26 21:20:38 -0300

    feat: create mapper to convert notification from prisma to domain

--- src/infra/database/prisma/mappers/prisma-notification-mapper.ts
+++ src/infra/database/prisma/mappers/prisma-notification-mapper.ts
@@ -1,4 +1,6 @@
 import { Notification } from '@application/entities/notification';
+import { NotificationContent } from '@application/entities/notification-content';
+import { Notification as PrismaNotification } from '@prisma/client';
 
 export class PrismaNotificationMapper {
   static toPrisma(notification: Notification) {
@@ -10,5 +12,19 @@ export class PrismaNotificationMapper {
       createdAt: notification.createdAt,
       readAt: notification.readAt,
     };
+  }
+
+  static toDomain(prismaNotification: PrismaNotification) {
+    return new Notification(
+      {
+        category: prismaNotification.category,
+        content: new NotificationContent(prismaNotification.content),
+        recipientId: prismaNotification.recipientId,
+        canceledAt: prismaNotification.canceledAt,
+        createdAt: prismaNotification.createdAt,
+        readAt: prismaNotification.readAt,
+      },
+      prismaNotification.id,
+    );
   }
 }
